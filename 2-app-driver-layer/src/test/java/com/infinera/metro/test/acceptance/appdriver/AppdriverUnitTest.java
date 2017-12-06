fd46bfa25bb95050b0f4bf242d7199310c27ca62
Patrik Bergman <patrik.bergman@infinera.com>
2017-12-06 15:26:00 +0100

    Trying to find why Jenkinsfile does not work

---
+++ 2-app-driver-layer/src/test/java/com/infinera/metro/test/acceptance/appdriver/AppdriverUnitTest.java
@@ -0,0 +1,17 @@
+package com.infinera.metro.test.acceptance.appdriver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("AppdriverUnitTest")
+@Slf4j
+class AppdriverUnitTest {
+
+    @DisplayName("Add node, get node and delete node")
+    @Test
+    void test() {
+        log.info("######## Running AppdriverUnitTest");
+
+    }
+}
